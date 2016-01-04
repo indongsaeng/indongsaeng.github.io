0c2f7b7a11e3e7877bb79d941ba00bbed7b13fc5
nasoy <nashbelandres@outlook.com>
2016-01-04 16:38:12 +0800

    adding send file php page

---
+++ send-file.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>JNB - Send file</title>
+</head>
+<body style="background:url('https://cdn01.boxcdn.net/cdn/farfuture/2Vk-AaS67bQLfkioTRysuuAbdhYlVNb3nd7NK11iCjs/md5:d62d35e3c050be6d06c88b1b429fa610/sites/default/files/homepage_redesign/Header_New_Compressed@2x.png') no-repeat; background-size:cover;">
+<div style="text-align: center;padding-top:100px;">
+
+<script type="text/javascript">var mf_dropbox_width=480;var mf_dropbox_id="e9b72d06a5bdef1f770ef9bff53dc69f5772804d4655580682999673c4e289b3";</script><script type="text/javascript" src="//www.mediafire.com/dropbox/dropbox.js"></script>
+	
+</div>
+</body>
+</html>
